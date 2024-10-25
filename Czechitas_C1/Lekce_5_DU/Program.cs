dda29028a5b8486f59836e85ce0253ecd53e8e54
S1eper <hlahulek.jan@gmail.com>
2024-10-25 18:59:33 +0200

    Založení nového solution s projektem
    Patrik je nejlepší!

---
+++ Czechitas_C1/Lekce_5_DU/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lekce_5_DU
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
