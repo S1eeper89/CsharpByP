d5a8fe75052bf42325e242ac234d135208e0ce76
S1eper <hlahulek.jan@gmail.com>
2024-11-20 19:27:52 +0100

    drobny update oprava poradi second number a zmena else if

--- Czechitas_C1/ProjektC#1/Program.cs
+++ Czechitas_C1/ProjektC#1/Program.cs
@@ -2,6 +2,7 @@
 {
     internal class Program
     {
+        // Metoda NactiOperator vypíše volitelné operátory a kontroluje platnost operátoru nebo výstupu, výstupem je string operator
         public static string NactiOperatorKonzole()
         {
             Console.WriteLine("Zadej operátor (+) (-) (*) (/) (^) nebo pro ukončení (x) ");
@@ -13,7 +14,7 @@
             }
             return firstOperator;
         }
-
+        // Metoda NactiDesetinneCisloZKonzole parsuje vstup uživatele a kontroluje, že číslo je slučitelné se zvoleným operátorem, vstupem je operátor
         public static string NactiDesetinneCisloZKonzole(string firstOperator)
         {
             string inputTwo = Console.ReadLine();
@@ -22,18 +23,20 @@
             while ((inputTwo.ToLower() != "x" && !double.TryParse(inputTwo, out secondNumber)) || (secondNumber == 0 && firstOperator == "/") || (firstOperator == "^" && secondNumber % 1 != 0))
             {
                 if (inputTwo == "x")
+                {
                     break;
-                if (secondNumber == 0 && firstOperator == "/")
+                }
+                else if (!double.TryParse(inputTwo, out secondNumber))
+                {
+                    Console.WriteLine("Nezadal jsi číslo zadej cislo, nebo (x) pro ukonceni");
+                }
+                else if (secondNumber == 0 && firstOperator == "/")
                 {
                     Console.WriteLine("Nulou nelze dělit, zadej číslo, nebo (x) pro ukonceni");
                 }
                 else if (firstOperator == "^" && secondNumber % 1 != 0)
                 {
                     Console.WriteLine("Nezadal jsi celé číslo, zadej celé číslo, nebo (x) pro ukonceni");
-                }
-                else
-                {
-                    Console.WriteLine("Nezadal jsi číslo zadej cislo, nebo (x) pro ukonceni");
                 }
                 inputTwo = Console.ReadLine();
             }
@@ -42,12 +45,14 @@
 
         static void Main(string[] args)
         {
+            // hlavní cyklus kalkulačky začínající nulou
             bool ukoncitProgram = false;
             while (!ukoncitProgram)
             {
+                // vytvoření první instance kalkulačky
                 Kalkulacka kalkulacka = new Kalkulacka();
                 Console.WriteLine($"Vysledek: {kalkulacka.VratAktualniVysledek()}");
-
+                // tryparsovaný první vstup, který je následně kontrolovaný ve while cyklu
                 Console.WriteLine("Zadej prvni cislo");
                 bool isFirstNumber = double.TryParse(Console.ReadLine(), out double firstNumber);
                 while (isFirstNumber == false)
@@ -56,31 +61,28 @@
                     isFirstNumber = double.TryParse(Console.ReadLine(), out firstNumber);
                 }
                 kalkulacka.Secti(firstNumber);
-
+                // spuštění while cyklu, kde se vykonávají operace mezi posledním výsledkem a novým číslem
                 string firstOperator = null;
-
                 while (firstOperator != "x")
                 {
-
-                    Console.WriteLine("Zadej operator.");
-
+                    // zadání prvního operátoru ve vnitřním cyklu
                     firstOperator = NactiOperatorKonzole();
                     if (firstOperator == "x")
                     {
                         Console.WriteLine($"Konečný výsledek = {kalkulacka.Result}");
                         break;
                     }
-
-                    Console.WriteLine("Zadej dalsi císlo, nebo x pro ukonceni");
-
+                    Console.WriteLine("Zadej dalsi císlo, nebo (x) pro ukonceni");
+                    // zadání druhého čísla, které provádí operace oproti poslednímu výsledku 
                     string inputSecondNumber = NactiDesetinneCisloZKonzole(firstOperator);
-
+                    // bohužel tady už potenciální celočíseln vstup opakovaně parsuji, protože mi prozí výstup přes x, nenapadlo mě elegantnější řešení :(
                     double.TryParse(inputSecondNumber, out double secondNumber);
+                    // if podmínka, která mění operátor pro vnitřní while cyklus, aby ho ukončila a započala kalkulačku se zadáním provní hodnoty od nuly
                     if (inputSecondNumber == "x")
                     {
                         firstOperator = "x";
                     }
-
+                    // blížíme se do cíle se switchem, který si na základě zadaného operátoru volá metody definované z logiky uložené v třídě Kalkulacka
                     switch (firstOperator)
                     {
                         case "+":
@@ -102,7 +104,7 @@
                     Console.WriteLine($"Vysledek: {kalkulacka.VratAktualniVysledek()}");
 
                 }
-                
+                //výstup z hlavního cyklu a ukončení programu
                 Console.WriteLine("Chcete ukončit program? ano / ne");
                 if (Console.ReadLine()?.ToLower() == "ano")
                 {
