9cd9fd8d2896b824e39f8d9def477f55e178b2a1
S1eper <hlahulek.jan@gmail.com>
2024-11-20 23:40:49 +0100

    nahrani ukolu 7 do separatniho projektu

---
+++ Czechitas_C1/DU_Lekce_7/Class1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Net;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DU_Lekce_7
+{
+
+    public class Kalkulacka
+    {
+        public double Result;
+        public Kalkulacka(double result = 0)
+        {
+            Result = result;
+        }
+        public bool JePlatnyOperator(string vstupOperator)
+        {
+            if (vstupOperator == "+" || vstupOperator == "-" || vstupOperator == "*" || vstupOperator == "/" || vstupOperator == "^")
+            {
+                return true;
+            }
+            else return false;
+        }
+        // ilustracni priklad jednoho reseni
+        public void Secti(double druheCislo)
+        {
+            double prvniCislo = Result;
+            Result = prvniCislo + druheCislo;
+        }
+        // ilustracni priklad jednodussiho reseni
+        public void Odecti(double prvniCislo)
+        {
+            Result -= prvniCislo;
+        }
+        public void Nasob(double prvniCislo)
+        {
+            Result *= prvniCislo;
+        }
+        public void Del(double prvniCislo)
+        {
+            Result /= prvniCislo;
+        }
+        public void Umocni(double prvniCislo)
+        {
+            if (prvniCislo == 0)
+            {
+                Result = 1;
+            }
+            else
+            {
+                Result = 1;
+                for (int i = 1; i <= (int)prvniCislo; i++)
+                {
+                    Result *= prvniCislo;
+                }
+            }
+        }
+        public double VratAktualniVysledek()
+        {
+            return Result;
+        }
+    }
+}
---
+++ Czechitas_C1/DU_Lekce_7/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace DU_Lekce_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kalkulacka kalkulacka = new Kalkulacka();
+            Console.WriteLine($"Vysledek: {kalkulacka.VratAktualniVysledek()}");
+
+            Console.WriteLine("Zadej prvni cislo");
+            bool isFirstNumber = double.TryParse(Console.ReadLine(), out double firstNumber);
+            //while (isFirstNumber == false)
+            //{
+            //    Console.WriteLine("Nezadal jsi cislo, zadej cislo.");
+            //    isFirstNumber = double.TryParse(Console.ReadLine(), out firstNumber);
+            //}
+            kalkulacka.Secti(firstNumber);
+
+            Console.WriteLine("Zadej prvni operator.");
+            string firstOperator = Console.ReadLine();
+            //bool jeOperator = (firstOperator == "+" || firstOperator == "-" || firstOperator == "*" || firstOperator == "/" || firstOperator == "^" || firstOperator.ToLower() == "x");
+            //while (!jeOperator)
+            //{
+            //    Console.WriteLine("Nezadal jsi spravny operator, zadej operátor (+) (-) (*) (/) (^)");
+            //    firstOperator = Console.ReadLine();
+            //    jeOperator = (firstOperator == "+" || firstOperator == "-" || firstOperator == "*" || firstOperator == "/" || firstOperator == "^" || firstOperator.ToLower() == "x");
+            //}
+            if (kalkulacka.JePlatnyOperator(firstOperator))
+            {
+                Console.WriteLine("Operator je platny");
+            }
+            else
+            {
+                Console.WriteLine("Operator je neplatny");
+                return;
+            }
+            bool isSecondNumber = double.TryParse(Console.ReadLine(), out double secondNumber);
+            switch (firstOperator)
+            {
+                case "+":
+                    kalkulacka.Secti(secondNumber);
+                    break;
+                case "-":
+                    kalkulacka.Odecti(secondNumber);
+                    break;
+                case "*":
+                    kalkulacka.Nasob(secondNumber);
+                    break;
+                case "/":
+                    kalkulacka.Del(secondNumber);
+                    break;
+                case "^":
+                    kalkulacka.Umocni(secondNumber);
+                    break;
+            }
+            Console.WriteLine($"Vysledek: {kalkulacka.VratAktualniVysledek()}");
+
+        }
+    }
+}
